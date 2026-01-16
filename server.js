d47bbd670d7a191b21859fea3c6aa5a5cb15650b
lakshyagoyal284 <lakshya.goyal2106206@gmail.com>
2026-01-16 13:08:27 +0530

    Fix CORS for Netlify

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['https://advosia-backend.onrender.com', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
